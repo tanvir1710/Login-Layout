8dc02b54caa93c1093bc0a507009b74df45d7243
tanvirc02 <tanvir.ahmed1710@gmail.com>
2023-12-17 21:44:45 +0100

    Final Commit

---
+++ app/src/main/res/drawable/LoginApp/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.loginapp.ui.theme.LoginAppTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var usernameInput : EditText
+    private lateinit var passwordInput :EditText
+    private lateinit var loginBtn : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener{
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials","Username : $username and Password : $password")
+        }
+
+
+    }
+}
+
